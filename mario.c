9467e13382a8640af33bbdc12ed34203a111caab
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-11 12:58:53 +0000

    ddd, Aug 8/11/2022, YYYY, 12:58 A +0

--- mario.c
+++ mario.c
@@ -1,6 +1,21 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    
+    do
+    {
+        int n = get_int("What size: ");
+    }
+    while (n < 1)
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
 }
