af81eee2cb6ba49a9b5f37829ab4c810b6ba700d
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-11 15:16:40 +0000

    ddd, Aug 8/11/2022, YYYY, 3:16 A +0

--- submits/mario-more/mario.c
+++ submits/mario-more/mario.c
@@ -6,6 +6,15 @@ int main(void)
     int n;
     do
     {
-        
+        n = get_int("Size of the pyramid: ");
+    }
+    while (n<1);
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf()
+        }
     }
 }
