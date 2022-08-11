fa86a4c2b36c181abc0d41740e3b2ef293335fc2
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-11 15:43:02 +0000

    ddd, Aug 8/11/2022, YYYY, 3:43 A +0

--- submits/mario-more/mario.c
+++ submits/mario-more/mario.c
@@ -12,7 +12,7 @@ int main(void)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = n - 1; j < n; j++)
         {
            int k = 0;
            while ()
