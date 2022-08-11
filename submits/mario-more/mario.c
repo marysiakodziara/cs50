ddb37305b7a7d744475d1cd5f10e31516f94d58d
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-11 16:53:31 +0000

    ddd, Aug 8/11/2022, YYYY, 4:53 A +0

--- submits/mario-more/mario.c
+++ submits/mario-more/mario.c
@@ -6,12 +6,44 @@ int main(void)
     int n;
     do
     {
-        n = get_int("Size of the pyramid: ");
+        n = get_int("Height of the pyramid: ");
     }
-    while (n < 1);
+    while (n < 1 || n > 8);
 
-    for (int i = 0; i < n; i++) //wysokość, enter
+    int x = n - 1;
+    int z = 1;
+
+    for (int i = 0; i < n; i++)
     {
-       
+        for (int j = 0; j < n; j++)
+        {
+            if (x > j)
+            {
+                 printf(" ");
+            }
+            else
+            {
+                 printf("#");
+            }
+        }
+
+        printf(" ");
+
+        for (int k = 0; k < n; k++)
+        {
+            if (z > k)
+            {
+                 printf("#");
+            }
+            else
+            {
+                 printf(" ");
+            }
+
+         }
+
+    printf("\n");
+    x--;
+    z++;
     }
 }
