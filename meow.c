dabb398d854cbb2ccf2bcbebebfc274fbe5fd9fe
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-11 10:30:30 +0000

    Thu, Aug 11, 2022, 10:30 AM +00:00

--- meow.c
+++ meow.c
@@ -1,15 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void meow(void)
-{
-    printf("meow\n");
-}
+void meow(int n);
 
 int main(void)
 {
+    meow(3);
+}
+
+void meow(int n)
+{
     for (int i = 0; i < 3; i++)
     {
-        meow();
+    printf("meow\n");
     }
 }
