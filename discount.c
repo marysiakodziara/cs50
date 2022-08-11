70ccef4e2017377792b7ad3449ec0ca90ecfc7fa
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-11 12:43:31 +0000

    ddd, Aug 8/11/2022, YYYY, 12:43 A +0

--- discount.c
+++ discount.c
@@ -6,12 +6,13 @@ float discount(float price);
 int main(void)
 {
     float regular = get_float("Regular Price: ");
+    int percent_off = get_int("Percent off: ");
     float sale = discount(regular);
     printf("Sale Price: %.2f\n", sale);
 }
 
-float discount(float price)
+float discount(float price, int percentage)
 {
-    return price * 0.85;
+    return price * percentage;
 
 }
