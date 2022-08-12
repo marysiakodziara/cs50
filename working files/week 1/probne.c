397a0559cf02abe507b377fba8a9c73d1e6c916c
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-12 12:49:19 +0000

    ddd, Aug 8/12/2022, YYYY, 12:49 A +0

--- working files/week 1/probne.c
+++ working files/week 1/probne.c
@@ -5,7 +5,7 @@ int main(void)
 {
    long n = get_int("What is the number? ");
    long i;
-   for (i = 1; i > n; i = i * 10)
+   for (i = 1; i < n; i = i * 10)
    {
     return i;
     printf("%li/n", i);
