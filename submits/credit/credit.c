2582270a4541b60bb77ac86fe5f6b65b91290364
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-12 16:58:05 +0000

    ddd, Aug 8/12/2022, YYYY, 4:58 A +0

--- submits/credit/credit.c
+++ submits/credit/credit.c
@@ -36,13 +36,13 @@ int digits_num(long x)
 
 int lunhs_algorithm(long x)
 {
-   int sum = 0;
+    int sum = 0;
     for (int i = 0; x > 0; i++)
     {
-         int every_other = (2*((x / 10) - ((x / 100) * 10)));
-         int prod_digits;
-         int remaining_numb = (x - ((x/10)*10));
-           if ( every_other > 9)
+           int every_other = (2 * ((x / 10) - ((x / 100) * 10)));
+           int prod_digits;
+           int remaining_numb = (x - ((x / 10) * 10));
+           if (every_other > 9)
            {
               prod_digits = (every_other / 10) + (every_other - ((every_other / 10)*10));
            }
