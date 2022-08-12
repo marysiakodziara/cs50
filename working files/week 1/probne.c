8ac819c9467d7ca3e153427141e8d1aa1a083a59
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-12 12:39:37 +0000

    ddd, Aug 8/12/2022, YYYY, 12:39 A +0

--- working files/week 1/probne.c
+++ working files/week 1/probne.c
@@ -3,15 +3,6 @@
 
 int main(void)
 {
-    int n;
-    do
-    {
-        n = get_int("Size of the pyramid: ");
-    }
-    while (n < 1 && n > 8);
 
-    for (int i = 0; i < n; i++) //wysokość, enter
-    {
-         
-    }
 }
+
