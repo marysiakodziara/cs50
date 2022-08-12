fba50e8a2faec3e43fbcc54fe7c9b3010463e1c7
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-12 15:40:37 +0000

    ddd, Aug 8/12/2022, YYYY, 3:40 A +0

--- working files/week 1/probne.c
+++ working files/week 1/probne.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int valid_number(long number, int wanted);
-int lunhs_algorithm(long number);
+int valid_number(long x, int wanted);
+int lunhs_algorithm(long x);
 
 
 int main(void)
@@ -12,34 +12,8 @@ int main(void)
 
    n = get_long("What is the number? ");
    digits = valid_number(n, digits);
+   
 
-   int suma = 0;
-   for (int i = 0; n > 0; i++)
-   {
-     int every_other = (2*((n / 10) - ((n / 100) * 10)));
-     int prod_digits;
-     int remaining_numb = (n - ((n/10)*10));
-     if( every_other > 9)
-     {
-        prod_digits = (every_other / 10 ) + (every_other - ((every_other / 10)*10));
-     }
-     else
-     {
-        prod_digits = every_other;
-     }
-
-     suma = suma + prod_digits + remaining_numb;
-     n = n / 100;
-   }
-printf("%i\n", suma);
-if ( suma % 10 == 0)
-{
-    printf("That is a credit card number\n");
-}
-else
-{
-    printf("Wrong number\n");
-}
 
 
 //if (digits < 13 || digits == 14 || digits > 16)
@@ -53,21 +27,47 @@ else
 //printf("%li \n", n);
 }
 
-int valid_number(long number, int wanted)
+int valid_number(long x, int wanted)
 {
-   while (number > 1)
+   while (x > 1)
    {
-    if (number > 10)
+    if (x > 10)
     {
         wanted++;
     }
-   number = number / 10;
+   x = x / 10;
    }
 
    return wanted;
 }
 
-int lunhs_algorithm(long number)
+int lunhs_algorithm(long x)
 {
-    
+   int suma = 0;
+   for (int i = 0; x > 0; i++)
+   {
+     int every_other = (2*((x / 10) - ((x / 100) * 10)));
+     int prod_digits;
+     int remaining_numb = (x - ((x/10)*10));
+     if( every_other > 9)
+     {
+        prod_digits = (every_other / 10 ) + (every_other - ((every_other / 10)*10));
+     }
+     else
+     {
+        prod_digits = every_other;
+     }
+
+     suma = suma + prod_digits + remaining_numb;
+     x = x / 100;
+   }
+printf("%i\n", suma);
+if ( suma % 10 == 0)
+{
+    printf("That is a credit card number\n");
+}
+else
+{
+    printf("Wrong number\n");
+}
 }
