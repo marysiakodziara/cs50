9898ef08a63935189ef83307796c097fd2d5d62d
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-12 13:10:30 +0000

    ddd, Aug 8/12/2022, YYYY, 1:10 A +0

--- working files/week 1/probne.c
+++ working files/week 1/probne.c
@@ -6,11 +6,12 @@ int main(void)
    long n = get_long("What is the number? ");
    int ten = 10;
    int digits = 0;
-   while (n > 1)
+   do
    {
     n = n / ten;
     digits++;
    }
+   while (n > 1);
 
    printf("%i\n", digits);
 }
