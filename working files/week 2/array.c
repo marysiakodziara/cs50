78e0c48da37774ce4ac50e6db87009ee8cea09ab
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-16 09:00:03 +0000

    Tue, Aug 16, 2022, 9:00 AM +00:00

--- working files/week 2/array.c
+++ working files/week 2/array.c
@@ -24,9 +24,113 @@ int main(void)
 int compute_score(string word)
 {
     int i = 0;
+    int suma = 0;
     while (word[i] != "\o")
     {
-        if(word[i] == "a")
+        if(word[i] == "a" || word[i] == "A")
+        {
+            suma = suma + POINTS[1];
+        }
+        if else (word[i] == "b" || word[i] == "B")
+        {
+            suma = suma + POINTS[2];
+        }
+        if else (word[i] == "c" || word[i] == "C")
+        {
+            suma = suma + POINTS[3];
+        }
+        if else (word[i] == "d" || word[i] == "D")
+        {
+            suma = suma + POINTS[4];
+        }
+        if else (word[i] == "e" || word[i] == "E")
+        {
+            suma = suma + POINTS[5];
+        }
+        if else (word[i] == "f" || word[i] == "F")
+        {
+            suma = suma + POINTS[6];
+        }
+        if else (word[i] == "g" || word[i] == "G")
+        {
+            suma = suma + POINTS[7];
+        }
+        if else (word[i] == "h" || word[i] == "H")
+        {
+            suma = suma + POINTS[8];
+        }
+        if else (word[i] == "i" || word[i] == "I")
+        {
+            suma = suma + POINTS[9];
+        }
+        if else (word[i] == "j" || word[i] == "J")
+        {
+            suma = suma + POINTS[10];
+        }
+        if else (word[i] == "k" || word[i] == "K)
+        {
+            suma = suma + POINTS[11];
+        }
+        if else (word[i] == "l" || word[i] == "L")
+        {
+            suma = suma + POINTS[12];
+        }
+        if else (word[i] == "m" || word[i] == "M")
+        {
+            suma = suma + POINTS[13];
+        }
+        if else (word[i] == "n" || word[i] == "N")
+        {
+            suma = suma + POINTS[14];
+        }
+        if else (word[i] == "o" || word[i] == "O")
+        {
+            suma = suma + POINTS[15];
+        }
+        if else (word[i] == "p" || word[i] == "P")
+        {
+            suma = suma + POINTS[16];
+        }
+        if else (word[i] == "r" || word[i] == "R")
+        {
+            suma = suma + POINTS[17];
+        }
+        if else (word[i] == "s" || word[i] == "S")
+        {
+            suma = suma + POINTS[18];
+        }
+        if else (word[i] == "t" || word[i] == "T")
+        {
+            suma = suma + POINTS[19];
+        }
+        if else (word[i] == "u" || word[i] == "U")
+        {
+            suma = suma + POINTS[20];
+        }
+        if else (word[i] == "w" || word[i] == "W")
+        {
+            suma = suma + POINTS[21];
+        }
+        if else (word[i] == "d" || word[i] == "D")
+        {
+            suma = suma + POINTS[22];
+        }
+        if else (word[i] == "d" || word[i] == "D")
+        {
+            suma = suma + POINTS[23];
+        }
+        if else (word[i] == "d" || word[i] == "D")
+        {
+            suma = suma + POINTS[24];
+        }
+        if else (word[i] == "d" || word[i] == "D")
+        {
+            suma = suma + POINTS[25];
+        }
+        if else (word[i] == "d" || word[i] == "D")
+        {
+            suma = suma + POINTS[26];
+        }
     }
 
     // TODO: Compute and return score for string
