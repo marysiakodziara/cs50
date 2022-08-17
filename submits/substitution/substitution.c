09a4fe3640dc7dc768f693e30cef8f1eb9bccb82
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-17 13:03:10 +0000

    Wed, Aug 17, 2022, 1:03 PM +00:00

--- submits/substitution/substitution.c
+++ submits/substitution/substitution.c
@@ -9,7 +9,11 @@ int main(int argc, string argv[])
    int command = strlen(argv[argc - 1]);
    if (argc == 2  && command == 26)
    {
-     command[]
+     string key = argv[1]
+     for (int i = 0; i < command; i++ )
+     {
+        if (isupper)
+     }
    } // tylko wtedy program puszcza dalej
    else if (argc == 1)
    {
