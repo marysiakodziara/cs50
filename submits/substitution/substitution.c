d442cfe4547caf883cc9ae494e0dbb73f91213af
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-17 15:18:06 +0000

    Wed, Aug 17, 2022, 3:18 PM +00:00

--- submits/substitution/substitution.c
+++ submits/substitution/substitution.c
@@ -9,54 +9,54 @@ int key_validation(string arg_v[], int arg_c);
 int main(int argc, string argv[])
 {
 
-   int open = key_validation(argv, argc);
-   if (open == 0)
-   {
-    printf("valid key\n");
-   }
-   else
-   {
-    printf("invalid key\n");
-   }
+    int open = key_validation(argv, argc);
+    if (open == 0)
+    {
+       printf("valid key\n");
+    }
+    else
+    {
+      printf("invalid key\n");
+    }
 
 }
 
 int key_validation(string arg_v[], int arg_c)
 {
-   int answer;
-   int command = strlen(arg_v[arg_c - 1]);
-   if (arg_c == 2  && command == 26)
-   {
-     int score = 0;
-     string key = arg_v[1];
-     for (int i = 0; i < command; i++ ) //sprawdzam czy wszystkie znaki są alfabetyczne
-     {
-        if (isupper(key[i]) == 0 && islower(key[i]) == 0)
+    int answer;
+    int command = strlen(arg_v[arg_c - 1]);
+    if (arg_c == 2  && command == 26)
+    {
+        int score = 0;
+        string key = arg_v[1];
+        for (int i = 0; i < command; i++ ) //sprawdzam czy wszystkie znaki są alfabetyczne
         {
-           score += 1;
-           if (score == 0)
+           if (isupper(key[i]) == 0 && islower(key[i]) == 0)
            {
-              int tab[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-              for (int k = 0; k < 26; k++)
+              score += 1;
+              if (score == 0)
               {
-                  if (isupper(key[k]))
-                  {
-                      tab[key[k] - 'A'] += 1;
-                  }
-                  else if (islower(key[k]))
-                  {
-                      tab[key[k] - 'a'] += 1;
-                  }
-              }
-              int tab2 = 1;
-              for (int z = 0; z < 26; z++)
-              {
-                      tab2 = tab2 * tab[z];
-              }
-               if (tab2 == 1)
-              {
+                 int tab[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                 for (int k = 0; k < 26; k++)
+                 {
+                     if (isupper(key[k]))
+                     {
+                        tab[key[k] - 'A'] += 1;
+                     }
+                     else if (islower(key[k]))
+                     {
+                        tab[key[k] - 'a'] += 1;
+                     }
+                 }
+                 int tab2 = 1;
+                 for (int z = 0; z < 26; z++)
+                 {
+                    tab2 = tab2 * tab[z];
+                 }
+                 if (tab2 == 1)
+                 {
                     answer = 0;
-              }
+                 }
            }
         }
      }
