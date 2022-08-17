f5019fcb370321781085eed2151ce262dcfcfb09
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-17 12:47:49 +0000

    Wed, Aug 17, 2022, 12:47 PM +00:00

--- submits/substitution/substitution.c
+++ submits/substitution/substitution.c
@@ -3,5 +3,8 @@
 
 int main(int argc, string argv[])
 {
-   argc == 2 // tylko wtedy program puszcza dalej
+   argc == 2  && strlen(argv[1]) == 26 // tylko wtedy program puszcza dalej
+   argc == 1 // program wyświetla Usage: ./substitution key
+   argc == 2 && strlen(argv[1]) != 26 // program wyświetla Key must contain 26 characters.
+   
 }
