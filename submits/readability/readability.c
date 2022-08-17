5498b18b2dd6cd28a39c3a25da8da8bdcfeeaa76
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-17 09:24:38 +0000

    Wed, Aug 17, 2022, 9:24 AM +00:00

--- submits/readability/readability.c
+++ submits/readability/readability.c
@@ -3,9 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string text);
 
 int main(void)
 {
-    string text = get_string("Write text sample: ");
+    string text1 = get_string("Write text sample: ");
     printf("%s\n", text);
+}
+
+int count_letters(string text)
+{
+    
 }
