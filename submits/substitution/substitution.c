3db12f9248885a9c6385fa48d65611dc5e605354
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-17 15:41:18 +0000

    Wed, Aug 17, 2022, 3:41 PM +00:00

--- submits/substitution/substitution.c
+++ submits/substitution/substitution.c
@@ -81,7 +81,7 @@ int key_validation(string arg_v[], int arg_c)
 
 string cipher(string text, string arg_v[])
 {
-   string tab3[26];
+   char tab3[26];
    string key = arg_v[1];
    int len = strlen(arg_v[1]);
    for (int i = 0; i < len; i++)
