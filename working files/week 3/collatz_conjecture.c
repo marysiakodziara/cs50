f7a4a0fd7429fc67b9638f702fc65926bc6e15b8
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-18 15:05:00 +0000

    Thu, Aug 18, 2022, 3:05 PM +00:00

--- working files/week 3/collatz_conjecture.c
+++ working files/week 3/collatz_conjecture.c
@@ -23,13 +23,13 @@ int collatz(int n)
 
 int sum(int n)
 {
-  int sum2 = 1;
+  int sum2 = 0;
+  sum2 += 1;
   int to_one = n;
   while (to_one != 1)
   {
     to_one = collatz(n);
   }
-      sum2 += 1;
 return sum2;
 }
 
