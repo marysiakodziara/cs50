c980d7b3e556d3db55a65de82f444d3740702ba3
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-18 14:55:48 +0000

    Thu, Aug 18, 2022, 2:55 PM +00:00

--- working files/week 3/collatz_conjecture.c
+++ working files/week 3/collatz_conjecture.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 int collatz(int n);
+int sum(int n);
 
 int main(void)
 {
+  int k2 = 0;
   int number = get_int("Give the number: ");
   int one = collatz(number);
   printf("%i\n", one);
@@ -18,5 +20,15 @@ int collatz(int n)
     return collatz(n/2);
     else
     return collatz(3 * n + 1);
+}
+
+int sum(int n);
+{
+  int sum2 = 1;
+  while (n != 1)
+  {
+    int one = collatz(number);
+    sum2 += 1;
+  }
 }
 
