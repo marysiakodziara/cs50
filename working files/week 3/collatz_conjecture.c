67497982d20754e7fdf7b6a170bbe4dd60799d14
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-18 15:23:29 +0000

    Thu, Aug 18, 2022, 3:23 PM +00:00

--- working files/week 3/collatz_conjecture.c
+++ working files/week 3/collatz_conjecture.c
@@ -16,8 +16,8 @@ int collatz(int n)
     if (n == 1)
     return 0;
     else if ((n % 2) == 0)
-    return collatz(n/2);
+    return 1 + collatz(n/2);
     else
-    return collatz(3 * n + 1);
+    return 1 + collatz(3 * n + 1);
 }
 
