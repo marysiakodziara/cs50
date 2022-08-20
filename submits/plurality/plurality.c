d3e922686c0ec869e1bffc6a8febec29239e907a
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-20 15:22:11 +0000

    Sat, Aug 20, 2022, 3:22 PM +00:00

--- submits/plurality/plurality.c
+++ submits/plurality/plurality.c
@@ -79,6 +79,11 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    
+    int k = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if{ }
+    }
+
     return;
 }
