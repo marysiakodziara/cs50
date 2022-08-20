6c6f0afd18cd1ec7de9aea51b0ef10a00be50152
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-20 10:45:14 +0000

    Sat, Aug 20, 2022, 10:45 AM +00:00

--- working files/week 3/struct.c
+++ working files/week 3/struct.c
@@ -2,10 +2,27 @@
 #include <stdio.h>
 
 typedef struct{
-
-} person; 
+    string name;
+    string number;
+} person;
 
 int main(void)
 {
+    person people[2];
 
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number  = "+1-949-468-2750";
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(people[i].name, "David") == 0)
+        {
+            printf("Found %s\n", poeple[i].number);
+            return 0;
+        }
+    }
+return 1;
 }
