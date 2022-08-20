ab1079e5b51d22dc58e45cfc4527f1d1fb1710c0
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-20 15:44:43 +0000

    Sat, Aug 20, 2022, 3:44 PM +00:00

--- submits/plurality/plurality.c
+++ submits/plurality/plurality.c
@@ -87,6 +87,10 @@ bool vote(string name)
 void print_winner(void)
 {
     int k = 0;
+    if (candidate_count == 1)
+    {
+        if 
+    }
     for (int i = 0; i < (candidate_count - 1); i++)
     {
         if (candidates[i].votes >= candidates[i+1].votes)
