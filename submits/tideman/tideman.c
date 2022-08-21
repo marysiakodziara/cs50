7e17378052b3c1deeca227e4449a04bb24737ce7
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-21 11:17:13 +0000

    Sun, Aug 21, 2022, 11:17 AM +00:00

--- submits/tideman/tideman.c
+++ submits/tideman/tideman.c
@@ -104,7 +104,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(candidates[i], name) == 0)
         {
-
+            ranks[i] = 
             return true;
         }
     }
