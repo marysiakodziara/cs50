78aedfc105fa5cbe9bf399693dd6bfb828ee6746
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-24 12:00:53 +0000

    Wed, Aug 24, 2022, 12:00 PM +00:00

--- submits/tideman/tideman.c
+++ submits/tideman/tideman.c
@@ -230,7 +230,7 @@ void lock_pairs(void)
 {
     for (int i = 0; i < pair_count; i++)
     {
-        if (!check_cycle(pairs[i].winner, pairs[i].loser))
+        if (!cycle_check(pairs[i].winner, pairs[i].loser))
         {
             locked[pairs[i].winner][pairs[i].loser] = true;
         }
