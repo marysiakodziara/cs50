808d4497a814680cbf10e4a6142848352dbf5f1b
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-28 11:38:10 +0000

    Sun, Aug 28, 2022, 11:38 AM +00:00

--- submits/filter-less/helpers.c
+++ submits/filter-less/helpers.c
@@ -3,7 +3,12 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    float 
+    float average;
+    for (int i = 0; i < height; i++)
+    {
+        average = (RGBTRIPLE[i].rgbtBlue + RGBTRIPLE[i].rgbtGreen + RGBTRIPLE[i].rgbtRed) / 3.0;
+        round 
+    }
     return;
 }
 
@@ -24,3 +29,10 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
 }
+
+
+    BYTE  rgbtBlue;
+    BYTE  rgbtGreen;
+    BYTE  rgbtRed;
+} __attribute__((__packed__))
+RGBTRIPLE;
