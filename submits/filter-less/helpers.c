db205bacddb3e1bb1fb4086d12aa38bc9f762891
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-08-28 14:45:03 +0000

    Sun, Aug 28, 2022, 2:45 PM +00:00

--- submits/filter-less/helpers.c
+++ submits/filter-less/helpers.c
@@ -105,7 +105,18 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             copy[i][j] = image[i][j];
-            if ()
+            if (i == 0 && j == 0)
+            {}
+            else if(i == 0 && j == (width - 1))
+            {}
+            else if(i == 0)
+            {}
+            else if(j == (width - 1) && i == (height - 1))
+            {}
+            else if(j == (width -1))
+            {}
+            else if()
+
         }
     }
 
