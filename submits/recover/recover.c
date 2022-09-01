8d3202ef6f3ae4c6566eec9c6fe3b7acaec1ca57
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-01 11:40:38 +0000

    Thu, Sep 1, 2022, 11:40 AM +00:00

--- submits/recover/recover.c
+++ submits/recover/recover.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("incorrect usage, type: ./recover 'file name'");
+        return 1;
+    }
+    FILE *inptr = fopen(argv[argc - 1], "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 1;
+    }
+
 }
