9d41e85d43fb459f55e9179a7c7642fbdc78145c
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-01 13:52:28 +0000

    Thu, Sep 1, 2022, 1:52 PM +00:00

--- submits/recover/recover.c
+++ submits/recover/recover.c
@@ -29,8 +29,8 @@ int main(int argc, char *argv[])
         {
             if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
             {
-                int j_count += 1;
-                sprintf(filename, "%03i.jpg", j_count)
+                j_count += 1;
+                sprintf(filename, "%03i.jpg", j_count);
                 FILE *img = fopen(filename, "w");
                 int k = i;
                 while ((buffer[k] != 0xff && buffer[k + 1] != 0xd8 && buffer[k + 2] != 0xff && (buffer[k + 3] & 0xf0) != 0xe0) && k < 512)
