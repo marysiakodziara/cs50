183f089406874761c3436ccc6ffcde98d69ff830
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-01 13:45:05 +0000

    Thu, Sep 1, 2022, 1:45 PM +00:00

--- submits/recover/recover.c
+++ submits/recover/recover.c
@@ -42,7 +42,14 @@ int main(int argc, char *argv[])
             }
             else if (j_count != 0)
             {
-
+                FILE *img = fopen(filename, "w");
+                int k = i;
+                while ((buffer[k] != 0xff && buffer[k + 1] != 0xd8 && buffer[k + 2] != 0xff && (buffer[k + 3] & 0xf0) != 0xe0) && k < 512)
+                {
+                    fwrite(buffer[k], 1, 1, filename);
+                    k++;
+                }
+                flose(filename);
             }
         }
     }
