777540bc2c68c91e3b6daf439a0e6dce0447264d
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-02 15:04:34 +0000

    Fri, Sep 2, 2022, 3:04 PM +00:00

--- submits/recover/recover.c
+++ submits/recover/recover.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
         printf("incorrect usage, type: ./recover 'file name'\n");
         return 1;
     }
-    FILE* file = fopen(argv[argc - 1], "r");
+    FILE *file = fopen(argv[argc - 1], "r");
     if (file == NULL)
     {
         printf("Could not open the file.\n");
@@ -23,36 +23,36 @@ int main(int argc, char *argv[])
     unsigned char buffer[512];
     char filename[8];
 
-    FILE* img = NULL;
-    while (fread (buffer, 1, 512, file) == 512)
+    FILE *img = NULL;
+    while (fread(buffer, 1, 512, file) == 512)
     {
 
-            if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (j_count == -1)
             {
-                if (j_count == -1)
-                {
-                    j_count += 1;
-                    sprintf(filename, "%03i.jpg", j_count);
-                    img = fopen(filename, "a");
-                    fwrite(buffer, 1 ,512, img);
-
-                }
-
-                else
-                {
-                    fclose(img);
-                    j_count += 1;
-                    sprintf(filename, "%03i.jpg", j_count);
-                    img = fopen(filename, "a");
-                    fwrite(buffer, 1 ,512, img);
-
-                }
-            }
-
-            else if (j_count != -1)
-            {
+                j_count += 1;
+                sprintf(filename, "%03i.jpg", j_count);
+                img = fopen(filename, "a");
                 fwrite(buffer, 1, 512, img);
+
             }
+
+            else
+            {
+                fclose(img);
+                j_count += 1;
+                sprintf(filename, "%03i.jpg", j_count);
+                img = fopen(filename, "a");
+                fwrite(buffer, 1, 512, img);
+
+            }
+        }
+
+        else if (j_count != -1)
+        {
+            fwrite(buffer, 1, 512, img);
+        }
 
     }
     fclose(img);
