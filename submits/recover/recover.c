ae70677c9731bb9fff1f709204566ef382976637
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-02 14:56:19 +0000

    Fri, Sep 2, 2022, 2:56 PM +00:00

--- submits/recover/recover.c
+++ submits/recover/recover.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
     //buffer for reading the file
     unsigned char buffer[512];
-    char filename[512];
+    char filename[8];
 
     FILE* img = NULL;
     while (fread (buffer, 1, 512, file) == 512)
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
             if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
             {
-                if (j_count == 0)
+                if (j_count == -1)
                 {
                     j_count += 1;
                     sprintf(filename, "%03i.jpg", j_count);
