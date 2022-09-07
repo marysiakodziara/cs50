15dfc7f82c49ceaf65923f81eeb8008fa526da49
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-07 11:40:20 +0000

    Wed, Sep 7, 2022, 11:40 AM +00:00

---
+++ working files/week 5/list2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+} node;
+
+int main(void)
+{
+    //list of size
+    node *list = NULL;
+
+}
