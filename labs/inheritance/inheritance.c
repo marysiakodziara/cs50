04e971b87f5f17ef765a9e01ee259d59d84661d8
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-08 10:24:08 +0000

    Thu, Sep 8, 2022, 10:24 AM +00:00

--- labs/inheritance/inheritance.c
+++ labs/inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    p = malloc(sizeof(person));
+    person *p = malloc(sizeof(person));
     if (p == NULL)
     {
         return 1;
