6f7e08a9fce4ed811d691dbad52b48ee0b10cf28
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-08 10:15:56 +0000

    Thu, Sep 8, 2022, 10:15 AM +00:00

--- labs/inheritance/inheritance.c
+++ labs/inheritance/inheritance.c
@@ -40,7 +40,11 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    
+    p = malloc(sizeof(person));
+    if (p == NULL)
+    {
+        return 1;
+    }
 
     // If there are still generations left to create
     if (generations > 1)
