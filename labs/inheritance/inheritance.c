ef2e57a0960e5b2199654bee9c8b6d04a657096a
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-08 14:22:10 +0000

    Thu, Sep 8, 2022, 2:22 PM +00:00

--- labs/inheritance/inheritance.c
+++ labs/inheritance/inheritance.c
@@ -84,10 +84,17 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p->parents == NULL)
+    {
+        return;
+    }
 
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
+    free(p);
 
 }
 
