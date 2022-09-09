0269d7b8301f2eba1127756164ed2be30e76d066
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-09 13:36:05 +0000

    Fri, Sep 9, 2022, 1:36 PM +00:00

--- working files/inheritance/inheritance.c
+++ working files/inheritance/inheritance.c
@@ -40,6 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
+    person *p = malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
@@ -49,8 +50,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        p->alleles[0] = p->parents[0]->alleles[rand() % 2];
+        p->alleles[1] = p->parents[1]->alleles[rand() % 2];
 
     }
 
