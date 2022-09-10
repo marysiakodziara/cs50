7acc5e3c06b4046a5e44af2538ad3416b6b29008
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-10 13:32:52 +0000

    Sat, Sep 10, 2022, 1:32 PM +00:00

--- submits/speller/dictionary.c
+++ submits/speller/dictionary.c
@@ -40,8 +40,16 @@ bool load(const char *dictionary)
     //fopen to dictionary
     //checks for null
 
+    //(loop i guess
+    
     //fscanf(file, "%s", word) it runs till reaching end of file
     //getting one string at a time, and returns EOF
+
+    //dma for each string
+    //checks for null
+    //copy string into node using strcpy
+
+    //)
 
 
 
