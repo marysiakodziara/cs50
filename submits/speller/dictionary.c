ee5411ea82b3bb03581a912d1193f20b62e4e74e
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-11 14:21:21 +0000

    Sun, Sep 11, 2022, 2:21 PM +00:00

--- submits/speller/dictionary.c
+++ submits/speller/dictionary.c
@@ -56,7 +56,7 @@ unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
     int count = 0;
-    for (int i = 0; i < LENGTH; i++)
+    for (int i = 0; i < (LENGTH + 1); i++)
     {
         if (strcmp(&word[i], "\0") != 0)
         {
