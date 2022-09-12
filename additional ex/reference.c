09e7ab2435fd5d16f998aff0e4977a55836ed2ca
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-12 12:58:11 +0000

    Mon, Sep 12, 2022, 12:58 PM +00:00

--- additional ex/reference.c
+++ additional ex/reference.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    
+    int x = get_int("first number:");
+    int y = get_int("second number:");
+
+    printf("The sum of %i and %i is %i\n", x, y, x + y);
 }
