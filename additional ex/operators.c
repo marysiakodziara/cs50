44657aa2f6519217057682f8bd608d8250fc1f16
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-12 12:33:43 +0000

    Mon, Sep 12, 2022, 12:33 PM +00:00

---
+++ additional ex/operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int m = 300;
+    float fx = 300.60006;
+    char cht = 'z';
+
+    printf("Pointer : Demonstrate the use of & and * operator :\n");
+    printf("--------------------------------------------------------\n");
+    printf("m = %i\nfx = %f\ncht = %c\n", m, fx, cht);
+
+    printf("Using & operator :\n");
+    printf("-----------------------\n");
+    printf("address of m = %p\naddress of fx = %p\naddress of cht = %p\n", &m, &fx, %cht);
+
+    
+
+    return 1;
+}
