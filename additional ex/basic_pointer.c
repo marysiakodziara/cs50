7ceb7d4568ba43c86c8e74974dfab715fa1305cf
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-12 12:11:04 +0000

    Mon, Sep 12, 2022, 12:11 PM +00:00

--- additional ex/basic_pointer.c
+++ additional ex/basic_pointer.c
@@ -4,7 +4,15 @@ int main(void)
 {
     int m = 10;
     int *z = &m;
+    int n;
+    int o;
+
     printf("z stores the address of m  = %p\n", z);
+    printf("*z stores the value of m = %i\n", *z);
+    printf("&m is the address of m = %p\n", &m);
+    printf("&n stores the address of n = %p\n", &n);
+    printf("&o  stores the address of o = %p\n", &o);
+    printf("&z stores the address of z = %p\n", &z);
 
     return 0;
 }
