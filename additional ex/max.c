3df9dc73df0deca7fec09ff0790491ffe6f614e5
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-12 13:08:11 +0000

    Mon, Sep 12, 2022, 1:08 PM +00:00

--- additional ex/max.c
+++ additional ex/max.c
@@ -5,6 +5,28 @@ int bigger(int *st, int *nd);
 
 int main(void)
 {
-    
+    int x = get_int("first number:");
+    int y = get_int("second number:");
+
+    int *st = &x;
+    int *nd = &y;
+
+    printf("%i is the maximum number.\n", bigger(st, nd));
+
 }
+
+int bigger(int *st, int *nd)
+{
+    if(*st > *nd)
+    {
+        return *st;
+    }
+    else
+    {
+        return *nd;
+    }
+
+    return 1;
+}
+
 
