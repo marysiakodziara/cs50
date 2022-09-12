769a7ac325b2570c019c199b7ca1280a6c9416ae
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-12 15:47:00 +0000

    Mon, Sep 12, 2022, 3:47 PM +00:00

---
+++ additional ex/recursion/count_up.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void count(int x);
+
+int main(void)
+{
+    int x = get_int("number : ");
+    printf("%i ", count(x));
+}
+
+void count(int x)
+{
+    if (x == 0)
+    {
+        return;
+    }
+    else
+    {
+        printf("%i, ", count(x - 1));
+    }
+}
