c9025d136b6dd432e79738f169d72e1b1e45749d
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-12 15:28:53 +0000

    Mon, Sep 12, 2022, 3:28 PM +00:00

--- additional ex/permutation.c
+++ additional ex/permutation.c
@@ -8,9 +8,11 @@ int main(void)
     int len = srtlen(word);
     for(int i = 0; i < len; i++)
     {
+        permutation(word, i);
     }
 }
 
-void permutation(string word,m int index)
+void permutation(string word, int index)
 {
+    
 }
