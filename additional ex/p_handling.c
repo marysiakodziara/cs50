8d00763ec5a6bfab2790b4e5358e10282cfa91bd
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-12 12:27:40 +0000

    Mon, Sep 12, 2022, 12:27 PM +00:00

--- additional ex/p_handling.c
+++ additional ex/p_handling.c
@@ -14,4 +14,9 @@ int main(void)
 
     printf("The value of m assigned to 34 now.\nAddress of pointer ab : %p\nContent of pointer ab : %i\n", ab, *ab);
 
+    *ab = 7;
+
+    printf("The pointer variable ab is assigned with the value 7 now.\nAddress of m : %p\nValue of m : %i\n", &m, m);
+
+    return 1;
 }
