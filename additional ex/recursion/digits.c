a61047eb348826a54b379ad09604af612ade6815
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-13 16:08:46 +0000

    Tue, Sep 13, 2022, 4:08 PM +00:00

--- additional ex/recursion/digits.c
+++ additional ex/recursion/digits.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int digit(int x);
+int digit(int x, int i);
 
 int main(void)
 {
-
+    int i = 0;
+    int x = get_int("number: ");
+    digit(x, i);
 }
 
-int digit(int x)
+int digit(int x, int i)
 {
-    static int i 
+    static int i
     if (x / 10 < )
 }
