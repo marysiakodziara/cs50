aca31d68b18f8a6cc63e1b101521734057a99069
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-13 11:11:45 +0000

    Tue, Sep 13, 2022, 11:11 AM +00:00

--- additional ex/recursion/fib.c
+++ additional ex/recursion/fib.c
@@ -15,14 +15,14 @@ int fib(int x)
     int k = 1;
     if (x > 1)
     {
-        x--;
-        sum = fib(x) + fib(x - 1);
+        sum += fib(x - 1);
         printf("%i ", sum);
     }
     else if (x == 1)
     {
+        sum = k;
         printf("%i ", k);
-        return k;
+        return sum;
     }
     else
     {
