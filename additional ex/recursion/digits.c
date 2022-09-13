4a4426b586faf822de8c2dc7152f84deceb81dad
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-13 16:31:55 +0000

    Tue, Sep 13, 2022, 4:31 PM +00:00

--- additional ex/recursion/digits.c
+++ additional ex/recursion/digits.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int digit(int x, int i);
+int digit(float x, int i);
 
 int main(void)
 {
     int i = 0;
-    int x = get_int("number: ");
+    float x = get_int("number: ");
     i = digit(x, i);
     printf("%i\n", i);
 
 }
 
-int digit(int x, int i)
+int digit(float x, int i)
 {
     if (x < 1)
     {
-        return 0;
+        return i;
     }
     else
     {
         i++;
         x = x/10;
-        printf("%i ", x);
+        printf("%f, %i\n ", x, i);
         digit(x, i);
     }
 
