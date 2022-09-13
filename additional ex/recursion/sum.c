b1ee1c12206ce7e00795a4c27d45ed8ce969047b
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-13 09:58:16 +0000

    Tue, Sep 13, 2022, 9:58 AM +00:00

--- additional ex/recursion/sum.c
+++ additional ex/recursion/sum.c
@@ -8,20 +8,21 @@ int main(void)
 {
     int x = get_int("input number: ");
     sum(x);
+    printf("sum is: %i\n", sumof);
+
+    return 1;
 }
 
 int sum(int x)
 {
-    if (x >= 1)
+    if (x > 0)
     {
+        sumof += x;
         x--;
         sum(x);
-        sumof += x;
-        printf("sum is: %i\n", sumof);
     }
     else
     {
-        x++;
         return x;
     }
 
