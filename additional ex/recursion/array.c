bd8d2757c00a23cd1982e6c5eab11a89c1292d94
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-13 15:24:24 +0000

    Tue, Sep 13, 2022, 3:24 PM +00:00

--- additional ex/recursion/array.c
+++ additional ex/recursion/array.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int print(int elem, int arr[elem]);
+int print(int elem, int arr[x]);
 
 int main(void)
 {
     int elem = get_int("number of elements: ");
     int arr[elem];
+    int index = elem - 1;
 
     for (int i = 0; i < elem; i++)
     {
         arr[i] = 2 + (2 * i);
     }
-    print(elem, &arr[elem]);
+    print(index, &arr[elem]);
 }
 
-int print(int elem, int arr[elem])
+int print(int elem, int arr[x])
 {
     if (elem == 0)
     {
+        printf("%i ", arr[elem]);
         return 0;
     }
     else
