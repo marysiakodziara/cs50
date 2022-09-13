b475479fe8f8aacbb8bf86ee9fc0a74dc84e50c9
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-13 16:47:22 +0000

    Tue, Sep 13, 2022, 4:47 PM +00:00

--- additional ex/recursion/dsum.c
+++ additional ex/recursion/dsum.c
@@ -6,11 +6,26 @@ int dsum(int x);
 
 int main(void)
 {
+    int x = get_int("number: ");
+    dsum(x);
+    printf("sum: %i\n", sum);
 
+    return 0;
 }
 
 int dsum(int x)
 {
-    
+    if (x < 10)
+    {
+        sum = sum + x;
+        return 0;
+    }
+    else
+    {
+        sum += x % 10;
+        x = x / 10;
+        dsum(x);
+    }
 
+    return 0;
 }
