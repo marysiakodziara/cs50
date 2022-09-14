8fbdea908495e67f2b03b135a16f1ed0b11ac137
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-14 13:55:12 +0000

    Wed, Sep 14, 2022, 1:55 PM +00:00

--- additional ex/binary_tree/implementation.c
+++ additional ex/binary_tree/implementation.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <cs50.h>
 
+typedef struct node
+{
+    int data;
+    struct node *left, *right;
+} node;
 
+node *create(void);
 
 int main(void)
 {
 
+}
+
+node *create(void)
+{
+    int x;
+    node *new_node;
+    new_node = malloc(sizeof(node));
+    
 }
