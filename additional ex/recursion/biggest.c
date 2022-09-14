3c08abfb2f575a342cf0a99f903b405388e71f6d
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-14 10:49:49 +0000

    Wed, Sep 14, 2022, 10:49 AM +00:00

--- additional ex/recursion/biggest.c
+++ additional ex/recursion/biggest.c
@@ -24,11 +24,11 @@ int biggest(int arr[], int x)
     static int i = 0;
     if (i == 0)
     {
-        bigger = arr[i];
         i++;
+        bigger = arr[i];
         return 0;
     }
-    else if (i < x - 1)
+    if (i < x - 1)
     {
         if (bigger > arr[i])
         {
