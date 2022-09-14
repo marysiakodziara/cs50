cf8f811a007f825012208fe7271bdd56b707c669
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-14 11:00:35 +0000

    Wed, Sep 14, 2022, 11:00 AM +00:00

---
+++ additional ex/recursion/reverse.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int reverse(int arr[], int x);
+
+int main(void)
+{
+
+}
+
+int reverse(int arr[], int x)
+{
+    static int i = 0;
+    int tmp;
+
+    if (i == x || i = x -1)
+    {
+        return 0;
+    }
+    else
+    {
+        tmp = arr[i];
+        arr[x - 1]
+    }
+}
