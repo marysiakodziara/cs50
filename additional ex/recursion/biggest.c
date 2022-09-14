ca98803793d508b1e224f46d22459611049d2af8
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-14 10:38:55 +0000

    Wed, Sep 14, 2022, 10:38 AM +00:00

--- additional ex/recursion/biggest.c
+++ additional ex/recursion/biggest.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <cs50.h>
 
-
-int biggest(int arr[]);
+int bigger = 0;
+int biggest(int arr[], int x);
 
 int main(void)
 {
 
 }
 
-int biggest(int arr[])
+int biggest(int arr[], int x)
 {
-    if
+    static int i = 0;
+    if (i == o)
+    {
+        bigger = arr[i];
+        i++;
+        return 0;
+    }
+    if (i < x)
+    {
+        
+    }
 }
