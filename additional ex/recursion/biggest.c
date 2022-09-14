b214dd42380c2746eb2d9fd6d9fffe23f36c16b7
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-14 10:48:26 +0000

    Wed, Sep 14, 2022, 10:48 AM +00:00

--- additional ex/recursion/biggest.c
+++ additional ex/recursion/biggest.c
@@ -6,13 +6,23 @@ int biggest(int arr[], int x);
 
 int main(void)
 {
+    int x = get_int("elements: ");
+    int arr[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = get_int("element %i: ", i);
+        printf("\n");
+    }
+    biggest(arr, x);
+    printf("the biggest number in array is: %i\n", bigger);
 
+    return 0;
 }
 
 int biggest(int arr[], int x)
 {
     static int i = 0;
-    if (i == o)
+    if (i == 0)
     {
         bigger = arr[i];
         i++;
