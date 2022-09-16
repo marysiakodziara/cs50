d31406b72207272cfdbcd93a471ba1f3eeaefbc6
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-16 13:04:47 +0000

    Fri, Sep 16, 2022, 1:04 PM +00:00

--- working files/week 6/agree.py
+++ working files/week 6/agree.py
@@ -3,7 +3,7 @@ from cs50 import get_string
 s = get_string("Do you agree? ")
 
 
-if s == "Y" or s == "y":
+if s in ["Y", "y"]:
     print("Agreed.")
 elif s == "N" or s =="n":
     print("Not agreed.")
