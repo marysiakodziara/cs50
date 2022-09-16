bab7685b6bc5da1969f395b9a316033e9330e2d1
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-16 15:55:14 +0000

    Fri, Sep 16, 2022, 3:55 PM +00:00

--- working files/week 6/phone2.py
+++ working files/week 6/phone2.py
@@ -1,12 +1,10 @@
 import csv
 from cs50 import get_string
 
-file = open("phone2.csv", "a")
+with open("phone2.csv", "a") as file:
 
 name = get_string("Name: ")
 number = get_string("Number: ")
 
 writer = csv.writer(file)
 writer.writerow([name, number])
-
-file.close()
