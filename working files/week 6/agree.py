6903ab8f01b0b03e137138b73b09e566e5d80a23
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-16 13:07:06 +0000

    Fri, Sep 16, 2022, 1:07 PM +00:00

--- working files/week 6/agree.py
+++ working files/week 6/agree.py
@@ -1,9 +1,9 @@
 from cs50 import get_string
 
-s = get_string("Do you agree? ")
+s = get_string("Do you agree? ").lower() # Binding two functions
 
 
-if s in ["Y", "y"]:
+if s in ["Y", "yes"]:
     print("Agreed.")
 elif s == "N" or s =="n":
     print("Not agreed.")
