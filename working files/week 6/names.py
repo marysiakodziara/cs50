b1077720f9100a99719d993682793307daa30613
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-16 15:26:35 +0000

    Fri, Sep 16, 2022, 3:26 PM +00:00

---
+++ working files/week 6/names.py
@@ -0,0 +1,10 @@
+import sys
+
+names = ["Bill", "Charlie", "Fred"]
+
+if "Fred" in names:
+    print("Found")
+    exit(0)
+
+print("Not found")
+exit(1)
---
+++ working files/week 6/phonebook.py
@@ -0,0 +1,6 @@
+from cs50 import get_string
+
+people = {
+    "Carter": "+1-617",
+    "David": "+1-949"
+}
