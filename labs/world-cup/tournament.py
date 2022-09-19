41800cf85957ca291959cff980ee5e7554cab332
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-19 15:23:03 +0000

    Mon, Sep 19, 2022, 3:23 PM +00:00

--- labs/world-cup/tournament.py
+++ labs/world-cup/tournament.py
@@ -13,9 +13,7 @@ def main():
     # Ensure correct usage
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
-    else:
-        print(sys.argv[1])
-
+        
     teams = []
     # TODO: Read teams into memory from file
     with open(sys.argv[1], 'r') as file:
