a4010d6f1d58132ae2779dd0bcfd373cbe640610
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-21 13:09:35 +0000

    Wed, Sep 21, 2022, 1:09 PM +00:00

--- submits/sentimental-mario-more/mario.py
+++ submits/sentimental-mario-more/mario.py
@@ -2,17 +2,18 @@
 n = input("What is the height? ")
 try:
     n = int(n)
-    while n out of range(1-8):
+    while n not in range(1-8):
         n = input("What is the height?")
-
+except:
+    n = input("What is the height?")
+    
 a = 1
 b = 1
 for p in range(n):
     print((n - b)*" " + a * "#" + "  " + a * "#")
     a += 1
     b += 1
-except:
-    n = input("What is the height?")
+
 
 
 
