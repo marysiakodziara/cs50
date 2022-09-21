b0673ce6ef61601351e02daa76d68494ceea55a0
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-21 12:56:38 +0000

    Wed, Sep 21, 2022, 12:56 PM +00:00

--- submits/sentimental-mario-more/mario.py
+++ submits/sentimental-mario-more/mario.py
@@ -1,16 +1,13 @@
 # TODO
-try:
-    n = int(input("What is the height? "))
-    while n > 8 or n < 1 or n != int(n):
-        n = int(input("What is the hight?"))
-    a = 1
-    b = 1
-    for p in range(n):
-        print((n - b)*" " + a * "#" + "  " + a * "#")
-        a += 1
-        b += 1
-except:
-    exit()
+n = input("What is the height? ")
+while n != int(n):
+    n = input("What is the hight?")
+a = 1
+b = 1
+for p in range(n):
+    print((n - b)*" " + a * "#" + "  " + a * "#")
+    a += 1
+    b += 1
 
 
 
