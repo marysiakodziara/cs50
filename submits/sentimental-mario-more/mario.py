de7ce68e0ca84f81c48dffcebd90532bb58a5a41
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-21 13:10:25 +0000

    Wed, Sep 21, 2022, 1:10 PM +00:00

--- submits/sentimental-mario-more/mario.py
+++ submits/sentimental-mario-more/mario.py
@@ -6,7 +6,10 @@ try:
         n = input("What is the height?")
 except:
     n = input("What is the height?")
-    
+    n = int(n)
+    while n not in range(1-8):
+        n = input("What is the height?")
+
 a = 1
 b = 1
 for p in range(n):
