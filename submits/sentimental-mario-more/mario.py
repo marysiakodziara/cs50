1bddeea7519c956b7df7d4b480dc159385c2629f
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-21 12:21:45 +0000

    Wed, Sep 21, 2022, 12:21 PM +00:00

--- submits/sentimental-mario-more/mario.py
+++ submits/sentimental-mario-more/mario.py
@@ -4,29 +4,10 @@ n = int(input("What is the height? "))
 while n > 8 or n < 1:
     n = int(input("What is the hight?"))
 
-x = n - 1
-z = 1
-
-for i in range(n):
-    for j in range(n):
-        if x > j:
-            print(" ")
-        else:
-            print("#")
-    print("  ")
-
-    # Right half
-    for k in range(n):
-        if z > k:
-            print("#")
-
-    #print("\n")
-    x += 1;
-    z += 1;
 a = 1
 b = 1
 
 for p in range(n):
-    print((x - b)*"." + a * "#" + "  " + a * "#")
+    print((n - b)*" " + a * "#" + "  " + a * "#")
     a += 1
     b += 1
