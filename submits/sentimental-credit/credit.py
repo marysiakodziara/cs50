a6c646b9602962cda45daa79e46953d903a7ac68
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-21 15:15:33 +0000

    Wed, Sep 21, 2022, 3:15 PM +00:00

--- submits/sentimental-credit/credit.py
+++ submits/sentimental-credit/credit.py
@@ -1,4 +1,5 @@
 # TODO
+from cs50 import get_long
 
 # checks for 15 numbers in cc number
 def digits_num(x):
@@ -23,3 +24,31 @@ def lunhs_algorithm(x):
         sum = sum + prod_digits + remaining_numb
         x = x / 100
 
+def answer(x, k, y)
+{
+    start_num = 0
+    while x > 99:
+        x = x / 10;
+        start_num = x;
+
+    if start_num / 10 == 4:
+        start_num = 4
+
+    if start_num == 4 and (k == 13 or k == 16) and y == 1:
+        print("VISA\n")
+    else if (start_num == 34 or start_num == 37) and k == 15 and y == 1:
+        print("AMEX\n")
+    else if (start_num == 51 or start_num == 52 or start_num == 53 or start_num == 54 or start_num == 55) and k == 16 and y == 1:
+        print("MASTERCARD\n")
+    else:
+        print("INVALID\n")
+
+
+n = get_long("What is the number? ")
+
+digits = digits_num(n)
+print(digits)
+
+validation = lunhs_algorithm(n)
+;
+answer(n, digits, validation);
