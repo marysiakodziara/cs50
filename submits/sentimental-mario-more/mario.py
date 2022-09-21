1e2944cc6ef72fb72a6965cd6516b2d5fb9e9a4b
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-21 12:09:04 +0000

    Wed, Sep 21, 2022, 12:09 PM +00:00

--- submits/sentimental-mario-more/mario.py
+++ submits/sentimental-mario-more/mario.py
@@ -7,20 +7,20 @@ while n > 8 or n < 1:
 x = n - 1
 z = 1
 
-    for i in range(n):
-        for j in range(n):
-            if x > j:
-                print(" ")
-            else:
-                print("#")
-        print("  ")
+for i in range(n):
+    for j in range(n):
+        if x > j:
+            print(" ")
+        else:
+            print("#")
+    print("  ")
 
-        # Right half
-        for k in range(n):
-            if z > k:
-                print("#")
+    # Right half
+    for k in range(n):
+        if z > k:
+            print("#")
 
-        print("\n")
-        x += 1;
-        z += 1;
+    print("\n")
+    x += 1;
+    z += 1;
 
