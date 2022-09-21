c8f51e25265450b2797d3b5eafcfcc0ecef4e1c0
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-21 12:02:10 +0000

    Wed, Sep 21, 2022, 12:02 PM +00:00

--- submits/sentimental-mario-more/mario.py
+++ submits/sentimental-mario-more/mario.py
@@ -1,7 +1,26 @@
 # TODO
 
-x = input("What is the hight?")
-while x > 8 or x < 1:
-    x = input("What is the hight?")
+n = input("What is the hight?")
+while n > 8 or n < 1:
+    n = input("What is the hight?")
 
+x = n - 1
+z = 1
+
+    for i in range(n):
+        for j in range(n):
+            if x > j:
+                print(" ")
+            else:
+                print("#")
+        print("  ")
+
+        # Right half
+        for k in range(n):
+            if z > k:
+                print("#")
+
+        print("\n")
+        x = x;
+        z++;
 
