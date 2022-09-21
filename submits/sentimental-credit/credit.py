d9225baf6d8b3cb7d3dc2c0f426e3e8f76d7bd87
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-21 14:58:24 +0000

    Wed, Sep 21, 2022, 2:58 PM +00:00

--- submits/sentimental-credit/credit.py
+++ submits/sentimental-credit/credit.py
@@ -17,3 +17,23 @@ int digits_num(long x)
 }
 
 #lunhs algorithm
+int lunhs_algorithm(long x)
+{
+    int sum = 0;
+    for (int i = 0; x > 0; i++)
+    {
+        int every_other = (2 * ((x / 10) - ((x / 100) * 10)));
+        int prod_digits;
+        int remaining_numb = (x - ((x / 10) * 10));
+        if (every_other > 9)
+        {
+            prod_digits = (every_other / 10) + (every_other - ((every_other / 10) * 10));
+        }
+        else
+        {
+            prod_digits = every_other;
+        }
+
+        sum = sum + prod_digits + remaining_numb;
+        x = x / 100;
+    }
