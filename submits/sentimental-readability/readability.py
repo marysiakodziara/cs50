49ba01e6cb682faf99a61890312d5014f4bdcce6
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-25 15:23:14 +0000

    Sun, Sep 25, 2022, 3:23 PM +00:00

--- submits/sentimental-readability/readability.py
+++ submits/sentimental-readability/readability.py
@@ -5,15 +5,16 @@ from cs50 import get_string
 def main():
     text1 = get_string("Write text sample: ")
     letters = count_letters(text1)
+    print(letters)
 
 
 def count_letters(text):
     n = len(text)
     letters = 0
     for i in range(n):
-        if text[i] in range (65, 90):
+        if char(text[i]) in range (65, 90):
             letters += 1
-        if text[i] in range (65, 90):
+        if text[i] in range (97, 122):
             letters += 1
     return letters
 
