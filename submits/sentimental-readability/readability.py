2411f5e5f00e36484171419f8a32403088a82e83
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-25 14:51:04 +0000

    Sun, Sep 25, 2022, 2:51 PM +00:00

--- submits/sentimental-readability/readability.py
+++ submits/sentimental-readability/readability.py
@@ -4,10 +4,11 @@ from sc50 import get_string
 
 def main():
     text1 = get_string("Write text sample: ")
+    
 
 
 def count_letters(text):
-    n = strlen(text)
+    n = len(text)
     letters = 0
     for i in range(n):
         if isupper(text[i]):
