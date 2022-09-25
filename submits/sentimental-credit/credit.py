45968174b3a04e7b86d9228728348132749d3976
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-25 12:15:33 +0000

    Sun, Sep 25, 2022, 12:15 PM +00:00

--- submits/sentimental-credit/credit.py
+++ submits/sentimental-credit/credit.py
@@ -4,8 +4,18 @@ from cs50 import get_int
 def main():
 
     n = get_int("the number is: )
-
+    digits = digits_num(x)
 
 
 def digits_num(x):
-    
+    k = 1
+    while x > 1:
+        if x > 10:
+            k += 1
+        x = x / 10
+
+    return k
+
+
+if __name__ == "__main__":
+    main()
