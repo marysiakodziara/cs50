ff592114239d2eb8f28bf0cd44a3edda4c42f56f
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-26 16:32:49 +0000

    Mon, Sep 26, 2022, 4:32 PM +00:00

--- submits/dna/dna.py
+++ submits/dna/dna.py
@@ -45,8 +45,8 @@ def main():
     # TODO: Check database for matching profiles
 
     for i in range(len(database)):
-        for j in range(len(database[i][j])):
-            database[i][j] = int(database[i][j])
+        for j in range(len(database[j])):
+            database[i][j] = int(database[j])
         print(database[i])
     return
 
