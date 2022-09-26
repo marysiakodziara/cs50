8b9abb14f9d5323b78a57fc2da30a640c2a0ed09
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-26 15:57:58 +0000

    Mon, Sep 26, 2022, 3:57 PM +00:00

--- submits/dna/dna.py
+++ submits/dna/dna.py
@@ -27,9 +27,8 @@ def main():
     with open(sys.argv[1], 'r') as file:
         firstLine = file.readline()
     firstLine = firstLine.strip('\n')
-    firstLine = firstLine.strip('name')
     sub = firstLine.split(',')
-    print(sub)
+    
 
 
     # TODO: Check database for matching profiles
