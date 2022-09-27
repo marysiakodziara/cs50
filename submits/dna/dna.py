63eee9697241fb81c411acac578c7975b3ec3358
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-27 10:07:42 +0000

    Tue, Sep 27, 2022, 10:07 AM +00:00

--- submits/dna/dna.py
+++ submits/dna/dna.py
@@ -43,7 +43,9 @@ def main():
     for i in range(len(database)):
         check = 0
         for j in range(1, subNum):
-            print(subDict[sub[j]], type(int(database[i][sub[j]])))
+            if subDict[sub[j]] == int(database[i][sub[j]]):
+                check += 1
+        print(check, )
 
 
 def longest_match(sequence, subsequence):
