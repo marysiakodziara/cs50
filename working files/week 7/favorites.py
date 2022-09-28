db3f83369ee3919aa876c141cd5035c294c143bb
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-28 12:21:49 +0000

    Wed, Sep 28, 2022, 12:21 PM +00:00

--- working files/week 7/favorites.py
+++ working files/week 7/favorites.py
@@ -6,10 +6,9 @@ with open("favorites.csv",  "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().upper()
-        if title in titles:
-            titles[title] += 1
-        else:
-            titles[title] = 1
+        if not title in titles:
+            titles[title] = 0
+        titles[title] += 1
 
 for title in titles:
     print(title, titles[title])
