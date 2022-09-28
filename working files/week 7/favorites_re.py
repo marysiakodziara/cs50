1b00bafb36e646028e0b7f2cb4dd1128174eac96
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-28 13:04:31 +0000

    Wed, Sep 28, 2022, 1:04 PM +00:00

--- working files/week 7/favorites_re.py
+++ working files/week 7/favorites_re.py
@@ -7,7 +7,7 @@ with open("favorites.csv",  "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().upper()
-        if re.search("MEOW", title):
+        if re.search("^(MEOW|THE MEOW)$", title):
             count += 1
 
 print(f"Meow was mentioned by {count} people")
