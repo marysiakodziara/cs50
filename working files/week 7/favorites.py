474e09fefdb67298793dbeb21dc04a8dc6ccf953
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-28 11:47:36 +0000

    Wed, Sep 28, 2022, 11:47 AM +00:00

--- working files/week 7/favorites.py
+++ working files/week 7/favorites.py
@@ -5,6 +5,7 @@ titles = []
 with open("favorites.csv",  "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
+        title = row["title"].strip().uppercase()
         if not row["title"] in titles:
             titles.append(row["title"])
 
