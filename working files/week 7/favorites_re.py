3b21a04541458bdf34a145dede288d82cf5e520c
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-28 13:03:05 +0000

    Wed, Sep 28, 2022, 1:03 PM +00:00

---
+++ working files/week 7/favorites_re.py
@@ -0,0 +1,13 @@
+import csv
+import re
+
+titles = {}
+count = 0
+with open("favorites.csv",  "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        title = row["title"].strip().upper()
+        if re.search("MEOW", title):
+            count += 1
+
+print(f"Meow was mentioned by {count} people")
