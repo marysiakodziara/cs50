d1c22b539c15aeeab0aa63e2e8c6ece34cd8ee5b
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-09-30 11:26:13 +0000

    Fri, Sep 30, 2022, 11:26 AM +00:00

--- labs/songs/4.sql
+++ labs/songs/4.sql
@@ -1,4 +1,4 @@
-SELECT name FROM songs;
-HAVING COUNT dencability > 0,75;
-HAVING COUNT energy > 0,75;
+SELECT name FROM songs
+HAVING COUNT dencability > 0,75
+HAVING COUNT energy > 0,75
 HAVING COUNT valence > 0,75;
