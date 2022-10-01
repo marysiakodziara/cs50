4b64d6b83bb36976d52f0d4ddce11fc7c7d4f610
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-01 15:31:22 +0000

    Sat, Oct 1, 2022, 3:31 PM +00:00

--- submits/movies/6.sql
+++ submits/movies/6.sql
@@ -1,1 +1,1 @@
-SELECT AVG(ratings.rating) FROM ratings JOIN movies ON movies.id = ratings.movie_id WHERE movies.year = 2018;
+SELECT AVG(ratings.rating) FROM ratings JOIN movies ON movies.id = ratings.movie_id WHERE movies.year = 2012;
