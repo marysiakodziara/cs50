4d4b2e68c12da860e6e3611f484c447f5315b027
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-01 16:34:47 +0000

    Sat, Oct 1, 2022, 4:34 PM +00:00

--- submits/movies/8.sql
+++ submits/movies/8.sql
@@ -1,2 +1,2 @@
-SELECT people.name FROM people JOIN stars, movies ON movies.id = stars.movie_id AND stars.person_id = people.id WHERE movies.title = "%Toy Story%";
+SELECT people.name FROM people JOIN stars, movies ON movies.id = stars.movie_id AND stars.person_id = people.id WHERE movies.title = "Toy Story";
 
