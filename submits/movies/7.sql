4d8c31487ece8b7f1b6e46cc284e6c96737ac443
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-01 16:06:13 +0000

    Sat, Oct 1, 2022, 4:06 PM +00:00

--- submits/movies/7.sql
+++ submits/movies/7.sql
@@ -3,6 +3,6 @@ movies realsid in 2010, their ratings (desc),
 SELECT movies.title, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id
 ORDER BY
 ( CASE
-    WHEN 
+    WHEN ratings.rating 
 
 )
