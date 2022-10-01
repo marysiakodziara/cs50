459eb7bad02238504b3b9c52376fa9bd4a22e0da
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-01 15:12:37 +0000

    Sat, Oct 1, 2022, 3:12 PM +00:00

--- submits/movies/6.sql
+++ submits/movies/6.sql
@@ -1,2 +1,2 @@
 SELECT movies.id, movies.title, movies.year, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id LIMIT 10
-SELECT AVG(ratings.rating) FROM ratings WHERE movies.year = 2012;
+COUNT AVG(ratings.rating) WHERE movies.year = 2012;
