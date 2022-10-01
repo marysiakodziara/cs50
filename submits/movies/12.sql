165da3293f8a63eb1d241079e4aa332270e013c7
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-01 17:13:56 +0000

    Sat, Oct 1, 2022, 5:13 PM +00:00

--- submits/movies/12.sql
+++ submits/movies/12.sql
@@ -1,1 +1,5 @@
-SELECT movies.title FROM movies JOIN stars, people 
+SELECT movies.title
+FROM movies
+JOIN stars, people
+ON movies.id = stars.movie_id AND stars.person_id = people.id
+WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
