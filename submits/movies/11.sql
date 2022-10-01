8831b42c488007224a9af0128a0e6fc244f1b07c
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-01 17:04:12 +0000

    Sat, Oct 1, 2022, 5:04 PM +00:00

--- submits/movies/11.sql
+++ submits/movies/11.sql
@@ -1,7 +1,6 @@
 SELECT movies.title FROM movies
 JOIN people, ratings, stars
 ON movies.id = stars.movie_id AND stars.person_id = people.id AND movies.id = ratings.movie_id
-ORDER BY ratings.rating
 WHERE people.name = "Chadwick Boseman"
-LIMIT 5;
-
+ORDER BY ratings.rating
+LIMIT 10;
