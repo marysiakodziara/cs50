a6d12be333ae306687be11d94ef49bc538830cf1
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-01 17:00:14 +0000

    Sat, Oct 1, 2022, 5:00 PM +00:00

---
+++ submits/movies/11.sql
@@ -0,0 +1,7 @@
+SELECT movies.title FROM movies
+JOIN people, ratings, stars
+ON movies.id = stars.movie_id AND stars.person_id = people.id AND movies.id = ratings.movie_id
+WHERE people.name = "Chadwick Boseman"
+ORDER BY ratings.rating
+LIMIT 5;
+
