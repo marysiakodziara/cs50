5865286630f139dbc5ec950d33a7efb692b59650
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 14:42:40 +0000

    Sun, Oct 2, 2022, 2:42 PM +00:00

--- submits/fiftyville/log.sql
+++ submits/fiftyville/log.sql
@@ -1,3 +1,3 @@
  -- Keep a log of any SQL queries you execute as you solve the mystery.
 \\ Im starting with looking through table crime_scene _reports as suggestedd in hints
-SELECT crime_scene_reports F
+SELECT year, month, day, street FROM crime_scene_reports WHERE year = 2021, month = 
