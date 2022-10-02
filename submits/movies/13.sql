6106a5405bbdd18ec1f715dbd5c1b499675b4dbe
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 09:43:31 +0000

    Sun, Oct 2, 2022, 9:43 AM +00:00

--- submits/movies/13.sql
+++ submits/movies/13.sql
@@ -1,2 +1,2 @@
 SELECT people.name FROM people JOIN stars, movies ON movies.id = stars.movie_id AND stars.person_id = people.id
-GROUP BY movies.title HAVING people.name = "Kevin Bacon" AND people.birth = 1958;
+WHERE people
