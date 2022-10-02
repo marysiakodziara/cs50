cc2ee3cad4e0fcc341a0b314b21198042b6bf838
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 12:30:26 +0000

    Sun, Oct 2, 2022, 12:30 PM +00:00

--- submits/movies/13.sql
+++ submits/movies/13.sql
@@ -5,3 +5,9 @@ UNION
 SELECT movies.id, people.name FROM movies
 JOIN stars, people ON movies.id = stars.movie_id AND stars.person_id = people.id
 WHERE people.name = "Kevin Bacon" AND people.birth = 1958;
+
+\\lil change with joining subquery
+
+SELECT movies.id, people.name FROM movies
+JOIN stars, people ON movies.id = stars.movie_id AND stars.person_id = people.id
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958;
