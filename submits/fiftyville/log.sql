873b4189cbe2c55d97d3e6b5c57d2a22303dfcd4
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 14:41:03 +0000

    Sun, Oct 2, 2022, 2:41 PM +00:00

--- submits/fiftyville/log.sql
+++ submits/fiftyville/log.sql
@@ -1,2 +1,3 @@
  -- Keep a log of any SQL queries you execute as you solve the mystery.
-\\ Im starting with looking through table
+\\ Im starting with looking through table crime_scene _reports as suggestedd in hints
+SELECT crime_scene_reports F
