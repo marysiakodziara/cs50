bba13d0611fefb4f559d3d899dc7e8d5f0cfc3db
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 09:22:15 +0000

    Sun, Oct 2, 2022, 9:22 AM +00:00

--- submits/movies/12.sql
+++ submits/movies/12.sql
@@ -2,6 +2,7 @@ SELECT movies.title
 FROM movies
 JOIN stars, people
 ON movies.id = stars.movie_id AND stars.person_id = people.id
-WHERE (people.name = "Johnny Depp" OR people.name = "Helena Bonham Carter") 
-HAVING COUNT(stars.movie_id) > 1;
+GROUP BY stars.movie_id
+WHERE (people.name = "Johnny Depp" OR people.name = "Helena Bonham Carter");
+
 
