6a4c1f222ccacd5d70126cf11a20f03beb232f56
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 11:12:28 +0000

    Sun, Oct 2, 2022, 11:12 AM +00:00

--- submits/movies/13.sql
+++ submits/movies/13.sql
@@ -3,5 +3,5 @@ FROM people
 JOIN stars, movies
 ON movies.id = stars.movie_id AND stars.person_id = people.id
 GROUP BY stars.person_id = (SELECT stars.person_id FROM stars JOIN people
-ON people.id = stars.person_id WHERE people.name = "Kevin Bacon" AND people.birth = 1958)
+ON people.id = stars.person_id WHERE people.name = "Kevin Bacon")
 AND people.name != "Kevin Bacon";
