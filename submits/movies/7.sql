aff1069a9eb8f3e8d7daadda79895c3dc345ae15
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 10:47:02 +0000

    Sun, Oct 2, 2022, 10:47 AM +00:00

--- submits/movies/7.sql
+++ submits/movies/7.sql
@@ -3,5 +3,8 @@ FROM movies
 JOIN ratings
 ON movies.id = ratings.movie_id
 WHERE movies.year = 2010
-ORDER BY 
+ORDER BY
+    CASE
+        WHEN COUNT(ratings.rating) > 1 THEN movies.title
+        ELSE DESC;
 
