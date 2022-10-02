672bdb7837410cfe942459d61a4f47d355467900
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 10:32:14 +0000

    Sun, Oct 2, 2022, 10:32 AM +00:00

--- submits/movies/7.sql
+++ submits/movies/7.sql
@@ -1,6 +1,1 @@
 SELECT movies.title, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id
-ORDER BY
-( CASE
-    WHEN ratings.rating HAVING COUNT(ratings.rating) > 1 THEN movies.title
-    ELSE DESC
-)
