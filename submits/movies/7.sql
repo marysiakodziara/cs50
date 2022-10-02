64a3e62ad29e696c0724077c1a5b4cfedaf731e9
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 10:29:01 +0000

    Sun, Oct 2, 2022, 10:29 AM +00:00

--- submits/movies/7.sql
+++ submits/movies/7.sql
@@ -3,6 +3,6 @@ movies realsid in 2010, their ratings (desc),
 SELECT movies.title, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id
 ORDER BY
 ( CASE
-    WHEN ratings.rating 
-
+    WHEN ratings.rating HAVING COUNT(ratings.rating) > 1 THEN movies.title
+    ELSE DESC
 )
