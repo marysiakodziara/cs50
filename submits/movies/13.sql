2320e7cca1f9cc7ba97c74662711b8655afb5e0d
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 11:50:41 +0000

    Sun, Oct 2, 2022, 11:50 AM +00:00

--- submits/movies/13.sql
+++ submits/movies/13.sql
@@ -1,7 +1,7 @@
 SELECT people.name FROM people
 JOIN stars
 ON people.id = stars.person_id
-GROUP BY stars.movie_id HAVING COUNT
-(SELECT movies.id, people.name FROM movies
+UNION
+SELECT movies.id, people.name FROM movies
 JOIN stars, people ON movies.id = stars.movie_id AND stars.person_id = people.id
-WHERE people.name = "Kevin Bacon" AND people.birth = 1958) = 1;
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958;
