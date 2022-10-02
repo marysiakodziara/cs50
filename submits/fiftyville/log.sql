5dc18c0d29cc28691703967e7544d8794124951e
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 14:45:34 +0000

    Sun, Oct 2, 2022, 2:45 PM +00:00

--- submits/fiftyville/log.sql
+++ submits/fiftyville/log.sql
@@ -1,3 +1,5 @@
  -- Keep a log of any SQL queries you execute as you solve the mystery.
 \\ Im starting with looking through table crime_scene _reports as suggestedd in hints
-SELECT year, month, day, street FROM crime_scene_reports WHERE year = 2021, month = 7, day = 28, street = "Humphrey Street";
+SELECT year, month, day, street
+FROM crime_scene_reports
+WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
