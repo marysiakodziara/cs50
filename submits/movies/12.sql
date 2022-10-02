133084f430ae206806e193e6f8b74b3f0a246d40
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 09:01:56 +0000

    Sun, Oct 2, 2022, 9:01 AM +00:00

--- submits/movies/12.sql
+++ submits/movies/12.sql
@@ -2,23 +2,5 @@ SELECT movies.title
 FROM movies
 JOIN stars, people
 ON movies.id = stars.movie_id AND stars.person_id = people.id
-WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
-
-SELECT movies.title, people.name FROM movies JOIN stars, people
-ON movies.title
-
-people.name = "Helena"
-people.name = "Johnny"
-
-\\duplicat with changes
-
-select movies where johnny depp was
-select movies where helena bonham carter was
-select movies.title that have count > 1
-
-SELECT movies.title
-FROM movies
-JOIN stars, people
-ON movies.id = stars.movie_id AND stars.person_id = people.id
 WHERE people.name = "Johnny Depp" OR people.name = "Helena Bonham Carter"
 HAVING COUNT(movies.title) > 1;
