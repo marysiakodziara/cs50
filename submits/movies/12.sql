31b0758666f11320c9ebd77a529dc82c9a4f206a
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 09:29:53 +0000

    Sun, Oct 2, 2022, 9:29 AM +00:00

--- submits/movies/12.sql
+++ submits/movies/12.sql
@@ -3,6 +3,6 @@ FROM movies
 JOIN stars, people
 ON movies.id = stars.movie_id AND stars.person_id = people.id
 WHERE (people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter")
-GROUP BY stars.movie_id;
+GROUP BY stars.movie_id HAVING
 
 
