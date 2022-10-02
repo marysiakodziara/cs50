4d1f07d6fd0f912d0422c5cff98078ae92d9bcf7
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 10:13:27 +0000

    Sun, Oct 2, 2022, 10:13 AM +00:00

--- submits/movies/13.sql
+++ submits/movies/13.sql
@@ -1,7 +1,7 @@
-SELECT *
+SELECT people.name
 FROM people
 JOIN stars, movies
 ON movies.id = stars.movie_id AND stars.person_id = people.id
-WHERE stars.movie_id = (SELECT stars.movie_id FROM stars JOIN people
+WHERE movies.id = (SELECT stars.movie_id FROM stars JOIN people
 ON people.id = stars.person_id WHERE people.name = "Kevin Bacon" AND people.birth = 1958)
 AND people.name != "Kevin Bacon";
