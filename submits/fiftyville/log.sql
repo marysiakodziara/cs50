927e0d6772972165a66b8ba27f48d89c6566a2b8
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-02 14:53:20 +0000

    Sun, Oct 2, 2022, 2:53 PM +00:00

--- submits/fiftyville/log.sql
+++ submits/fiftyville/log.sql
@@ -7,4 +7,4 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
 \\description states that three witnesses were interviewed, im gonna use this clue and check interviews table
 SELECT transcript
 FROM interviews
-WHERE 
+WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street"; 
