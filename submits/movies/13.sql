cd465fa5f3a6c80dd26b0e10015cf68bcc3a0ae0
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-17 14:13:19 +0000

    Mon, Oct 17, 2022, 2:13 PM +00:00

--- submits/movies/13.sql
+++ submits/movies/13.sql
@@ -1,9 +1,9 @@
 SELECT people.name
 FROM people
-JOIN stars, movies
-ON people.id = stars.person_id AND stars.movie_id = movies.id
-WHERE movies.id = (SELECT movies.id
-FROM movies
-JOIN stars, people
-ON movies.id = stars.movie_id AND stars.person_id = people.id
-WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
+INNER JOIN
+stars as s
+ON people.id = s.person_id
+INNER JOIN
+movies as m
+ON s.movie_id = m.id
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958;
