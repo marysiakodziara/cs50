8c442d3db292b1c26dcf8e7d72ad207fa80a1153
marysiakodziara <108337274+marysiakodziara@users.noreply.github.com>
2022-10-17 15:45:54 +0000

    Mon, Oct 17, 2022, 3:45 PM +00:00

--- submits/movies/13.sql
+++ submits/movies/13.sql
@@ -6,4 +6,5 @@ WHERE movies.id IN (SELECT movies.id
                     FROM movies
                     JOIN stars, people
                     ON movies.id = stars.movie_id AND stars.person_id = people.id
-                    WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
+                    WHERE people.name = "Kevin Bacon" AND people.birth = 1958)
+    AND people.name != "Kevin Bacon";
